b70119e6f1ccc633be8081948d5a0b26f1d4384b
Kr0nys <144237820+Kr0nys@users.noreply.github.com>
2023-09-11 10:36:03 +0300

    Update Блок4_зад28.pas

--- Блок4_зад28.pas
+++ Блок4_зад28.pas
@@ -1,14 +1,11 @@
-﻿Program Block4_Zad28;
+Program Block4_Zad28;
 var n: integer;
 begin
   write('Введите число коров: ');
   read(n);
-  if n>100 then write('Число коров не должно быть больше 100')
-  else begin
-    if n=1 then write('На лугу пасется ', n ,' korova') else begin
-       if n<=4 then write('На лугу пасется ', n,' korovy') else begin
-         if n<=100 then write('На лугу пасется ', n,' korov') else;
-       end;
-    end;
-  end;
+ case n of
+   0,5..10,12..20,25..30,35..40,45..50,55..60,65..70,75..80,85..90,95..100 : write('На лугу пасется ',n,' korov');
+   1,21,31,41,51,61,71,81,91: write('На лугу пасется ',n,' korova');
+   else write('На лугу пасется ',n,' korovy');
+ end;
 end.

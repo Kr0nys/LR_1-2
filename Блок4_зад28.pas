e8b6aebfe2adacc8c32377a968bca54287fd7c3e
Kr0nys <144237820+Kr0nys@users.noreply.github.com>
2023-09-11 11:18:29 +0300

    Update Блок4_зад28.pas

--- Блок4_зад28.pas
+++ Блок4_зад28.pas
@@ -1,11 +1,13 @@
 Program Block4_Zad28;
 var n: integer;
+
 begin
   write('Введите число коров: ');
   read(n);
- case n of
-   0,5..10,12..20,25..30,35..40,45..50,55..60,65..70,75..80,85..90,95..100 : write('На лугу пасется ',n,' korov');
-   1,21,31,41,51,61,71,81,91: write('На лугу пасется ',n,' korova');
+  if (n>=10) and (n<=20) then write('На лугу пасется ',n,' korov') else
+ case n mod 10 of
+   0,5..9 : write('На лугу пасется ',n,' korov');
+   1: write('На лугу пасется ',n,' korova');
    else write('На лугу пасется ',n,' korovy');
  end;
 end.
